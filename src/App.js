0b2e62209fe90bdf6da2484dc70e316bfe5674ca
Shanjida Nahid Shorna <sanjidanahid53@gmail.com>
2023-11-13 13:06:14 +0600

    modification

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Upload from './artifacts/contracts/Upload.sol/Upload.json';
 import {useEffect,useState} from "react";
@@ -10,19 +9,54 @@ import Fileupload from './components/Fileupload';
 
 function App() {
 
-  const [acccounts,setAccount]=useState("");
+  const [account,setAccount]=useState("");
   const[contract,setContract]=useState(null);
   const[provider ,setProvider]=useState(null);
   const [modalOpen,setModalOpen]=useState(false);
 
   useEffect(()=>{
     const provider=new ethers.providers.Web3Provider(window.ethereum);
-    
-  })
+
+  
+  const loadprovider=async()=>{
+    if (provider) {
+      window.ethereum.on("chainChanged", () => {
+        window.location.reload();
+      });
+
+      window.ethereum.on("accountsChanged", () => {
+        window.location.reload();
+      });
+      await provider.send("eth_requestAccounts",[]);
+      const signer=provider.getSigner();
+      const address=await signer.getAddress();
+      setAccount(address);
+      let constractAddress="0x5FbDB2315678afecb367f032d93F642f64180aa3";
+      const contract=new ethers.Contract(constractAddress,Upload.abi,signer);
+      
+      console.log(contract);
+      setContract(contract);
+      setProvider(provider);
+
+    }
+    else{
+      console.log("error");
+    }
+  };
+ 
+  //console.log("Hello");
+  provider && loadprovider();
+},[]);
+
 
   return (
     <div className="App">
-      
+      <h1 >Gdrive 3.0</h1>
+      <div class="bg"></div>
+      <div class="bg 1"></div>
+      <div class="bg 2"></div>
+      <p>Account : {account ? account : "Please connect Metamask"}</p>
+      <Fileupload account={account} provider={provider} contract={contract}></Fileupload>
     </div>
   );
 }
--- src/components/Fileupload.js
+++ src/components/Fileupload.js
@@ -1,4 +1,52 @@
-const Fileupload =()=>{
+// import { useState } from "react";
+// import axios from "axios";
+// import "./Fileupload.css";
 
-}
+// const Fileupload =({contract,account,provider})=>{
+//      const handleSubmit=async(e)=>{
+//      };
+//     const retrieveFile=(e)=>{
+//     };
+//     return (<div>
+//          <form className="form" onSubmit={handleSubmit}>
+//             <label htmlFor="file-upload" className="choose">
+//                 choose image
+//             </label>
+//             <input disabled={account} type="file " id="file-upload" name="data"onChange={retrieveFile}> </input>
+//             {/* <span className="textArea">Image:#temporary.png</span> */}
+//             <button type="submit" className="upload"></button>
+//         </form>
+//     </div>)
+
+// }
+// export default Fileupload;
+
+import { useState } from "react";
+import axios from "axios";
+import "./Fileupload.css";
+
+const Fileupload = ({ contract, account, provider }) => {
+  const handleSubmit = async (e) => {};
+  const retrieveFile = (e) => {};
+
+  return (
+    <div>
+      <form className="form" onSubmit={handleSubmit}>
+        <label htmlFor="file-upload" className="choose">
+          Choose image
+        </label>
+
+        <input
+          disabled={account}
+          type="file"
+          id="file-upload"
+          name="data"
+          onChange={retrieveFile}
+        />
+        <button type="submit" className="upload">Upload File</button>
+      </form>
+    </div>
+  );
+};
+
 export default Fileupload;
