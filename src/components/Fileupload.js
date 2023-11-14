630261dfef08d085e0deb507d95c371d0d33e114
Shanjida Nahid Shorna <sanjidanahid53@gmail.com>
2023-11-14 16:14:01 +0600

    modified

--- src/components/Fileupload.js
+++ src/components/Fileupload.js
@@ -1,32 +1,27 @@
-// import { useState } from "react";
-// import axios from "axios";
-// import "./Fileupload.css";
-
-// const Fileupload =({contract,account,provider})=>{
-//      const handleSubmit=async(e)=>{
-//      };
-//     const retrieveFile=(e)=>{
-//     };
-//     return (<div>
-//          <form className="form" onSubmit={handleSubmit}>
-//             <label htmlFor="file-upload" className="choose">
-//                 choose image
-//             </label>
-//             <input disabled={account} type="file " id="file-upload" name="data"onChange={retrieveFile}> </input>
-//             {/* <span className="textArea">Image:#temporary.png</span> */}
-//             <button type="submit" className="upload"></button>
-//         </form>
-//     </div>)
-
-// }
-// export default Fileupload;
-
 import { useState } from "react";
 import axios from "axios";
 import "./Fileupload.css";
 
 const Fileupload = ({ contract, account, provider }) => {
-  const handleSubmit = async (e) => {};
+  const[file,setFile]=useState(null);
+  const[fileName,setFileName]=useState("No image selected");
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(file){
+      try{
+       
+       const formData = new FormData();
+
+
+      }catch(e){
+        alert("Unable to upload to pinata");
+
+      }
+    }
+
+  };
   const retrieveFile = (e) => {};
 
   return (
@@ -37,12 +32,13 @@ const Fileupload = ({ contract, account, provider }) => {
         </label>
 
         <input
-          disabled={account}
+          disabled={!account}
           type="file"
           id="file-upload"
           name="data"
           onChange={retrieveFile}
         />
+        <span className="textArea">Image:{fileName}</span>
         <button type="submit" className="upload">Upload File</button>
       </form>
     </div>
@@ -50,3 +46,4 @@ const Fileupload = ({ contract, account, provider }) => {
 };
 
 export default Fileupload;
+
